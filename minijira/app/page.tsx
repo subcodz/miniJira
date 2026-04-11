8b7f732dd2b9f0cd0c0a25d89b3bf0510c272907
Subhajit Baidya <subhajitsb923692@gmail.com>
2026-04-11 21:33:12 +0530

    Initial commit

---
+++ minijira/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center font-sans dark:bg-black">
+      
+      <Button variant={"secondary"}>Hey there!</Button>
+    </div>
+  );
+}
